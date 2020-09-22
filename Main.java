013633703be27d68ced4262a2f35710c6f2db489
ddesai05 <dylan.desai@patelhs.org>
2020-09-22 15:56:27 +0000

    i added if and else statements saying i am 15 years old

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    int age = 15;
+  
+    if (age == 15)
+    {
+      System.out.println("You are 15 years old.");
+    }else{
+      System.out.println("You are not 15 years old");
+    }
   }
-}
+}   
